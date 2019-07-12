d060acf969ea67138bf50735633548ea9178377d
“Dean” <467271883@qq.com>
2019-07-12 15:55:44 +0800

    Add Test case for FizzBuzz convert function which should return Fizz given 3
    Test case By Smith Peng
    Impletement By Dean Huang

--- src/main/java/com/thoughtworks/tdd/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzz.java
@@ -2,6 +2,6 @@ package com.thoughtworks.tdd;
 
 public class FizzBuzz {
     public String convert(int number){
-        return number + "";
+        return number % 3 == 0 ? "Fizz" : number + "";
     }
 }
--- src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
@@ -8,7 +8,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class FizzBuzzTest {
 
     @Test
-    public void should_return_1_when_convert_given_1(){
+    public void should_return_1_when_convert_given_1() {
         //given
         int input = 1;
         //when
@@ -17,4 +17,22 @@ public class FizzBuzzTest {
         assertThat(result, is("1"));
     }
 
+    @Test
+    public void should_return_2_when_convert_given_2() {
+        //given
+        int input = 2;
+        //when
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result = fizzBuzz.convert(input);
+        assertThat(result, is("2"));
+    }
+    @Test
+    public void should_return_Fizz_when_convert_given_3(){
+        //given
+        int input = 3;
+        //when
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result = fizzBuzz.convert(input);
+        assertThat(result, is("Fizz"));
+    }
 }
